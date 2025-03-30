279bd854b75af4fad23719e6d76ecaed09edbf80
Fitra Ramadhan S <fitraramadhan668@gmail.com>
2025-03-30 15:01:17 +0700

    feat : Docker File

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: 'localhost', 
+    host: '34.70.34.221', 
     user: 'root',      
     password: '',      
     database: 'prak_cc'
